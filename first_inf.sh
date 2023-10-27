35a966b2f2f5a5d13cfa9df8f74181e0eff4b452
RepinaZona <An.repina.na@gmail.com>
2023-10-27 10:16:49 +0300

    add file

--- first_inf.sh
+++ first_inf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ####Colors####
+
 #Red thin font#
 R='\e[0;31m'
 #Red bold font#
@@ -22,30 +23,47 @@ W='\e[0m'
 W1='\e[1;0m'
 
 #### Variables ####
+NA="N/A"
 
 #### Information about MotherBoard####
 
-MPM=`dmidecode --type baseboard | grep "Product Name" | awk '{print $3}'`
-MSN=`dmidecode --type baseboard | grep "Serial Number" | awk '{print $3}'`
+#MPM=`dmidecode --type baseboard | grep "Product Name" | awk '{print $3}'`
+#MSN=`dmidecode --type baseboard | grep "Serial Number" | awk '{print $3}'`
 #MBV=`dmidecode --type baseboard | grep  "Version" | awk '{print $2}'`
-#TEST=`echo "HEELLOOOOO"`
+
 
 #### Information about BIOS####
 
-#BVEN=`dmidecode -t bios | grep "Vendor" | sed -e 's/-dev//'`
-BVER=`dmidecode -t bios | grep "Version" | sed -e 's/-dev//'`
-#BREL=`dmidecode -t bios | grep "Release" | awk '{print $2" "$3}' | sed -e 's/-dev//'`
+BVER=`dmidecode -t bios | grep "Version" | awk -F"Version: " '{print $2}'`
+if [[ $BVER = '' ]] ; then BVER=$NA; fi
+BREL=`dmidecode -t bios | grep "Release" | awk -F"Release Date: " '{print $2}'`
+if [[ $PNU_FR = '' ]] ; then PNU_FRU=$NA; fi
 
 #### Information about BMC ####
 
-IP_BMC=`ipmitool lan print | grep "IP Address" | awk '{print $4}' | tr -d : | sed 's/   //'`
-M1_BMC=`ipmitool raw 0x3a 0xa1 0x00`
-M8_BMC=`ipmitool raw 0x3a 0xa1 0x01`
-PN_BMC=`ipmitool fru | grep " Product Serial" | awk '{print $4}'`
-PN_FRU=`echo "Graviton"`
-PNU_FRU=`ipmitool fru print 3 | grep "Product Part Number" | awk '{print $3}'`
-PV_FRU=`ipmitool fru print 3 | grep "Product Version" | awk '{print $3}' | sed 's/://'`
-PS_FRU=`ipmitool fru print 3 | grep "Product Serial" | awk '{print $3}'`
+BMCVER=`ipmitool mc info | grep "Firmware Revision" | awk -F"Firmware Revision         : " '{print $2}'`
+if [[ $BMCVER = '' ]] ; then BMCVER=$NA; fi
+IP_BMC=`ipmitool lan print | grep "IP Address              :" | awk -F"IP Address              : " '{print $2}'`
+if [[ $IP_BMC = '' ]] ; then IP_BMC=$NA; fi
+M1_BMC=`ipmitool raw 0x3a 0xa1 0x00 | awk '{sub(/^[ \t]+/, ""); print }'`
+if [[ $M1_BMC = '' ]] ; then M1_BMC=$NA; fi
+M2_BMC=`ipmitool raw 0x3a 0xa1 0x01 | awk '{sub(/^[ \t]+/, ""); print }'`
+if [[ $M2_BMC = '' ]] ; then M2_BMC=$NA; fi
+
+FRU_PRINT=`ipmitool fru print | xxd -p -c 999999999`
+PN_FRU=`echo $FRU_PRINT | xxd -r -p | grep "Product Name" | awk -F"Product Name          : " '{print $2}'`
+if [[ $PN_FRU = '' ]] ; then PN_FRU=$NA; fi
+PNU_FRU=`echo $FRU_PRINT | xxd -r -p | grep "Product Part Number" | awk -F"Product Part Number   : " '{print $2}'`
+if [[ $PNU_FRU = '' ]] ; then PNU_FRU=$NA; fi
+PS_FRU=`echo $FRU_PRINT | xxd -r -p | grep "Product Serial" | awk -F"Product Serial        : " '{print $2}'`
+if [[ $PS_FRU = '' ]] ; then PS_FRU=$NA; fi
+MP_FRU=`echo $FRU_PRINT | xxd -r -p | grep "Board Product" | awk -F"Board Product         : " '{print $2}'`
+if [[ $MP_FRU = '' ]] ; then MP_FRU=$NA; fi
+MS_FRU=`echo $FRU_PRINT | xxd -r -p | grep "Board Serial" | awk -F"Board Serial          : " '{print $2}'`
+if [[ $MS_FRU = '' ]] ; then MS_FRU=$NA; fi
+MMF_FRU=`echo $FRU_PRINT | xxd -r -p | grep "Board Mfg Date" | awk -F"Board Mfg Date        : " '{print $2}'`
+if [[ $MMF_FRU = '' ]] ; then MMF_FRU=$NA; fi
+
 
 #### PRINT #####
 
@@ -53,48 +71,43 @@ echo -e ${O1}"  _____________________________________________________"
 echo -e ${O1}" |                                                     |"
 echo -e ${O1}" |           Your server has these parameters :        |"
 echo -e ${O1}" |_____________________________________________________|"
-echo -e ${W} " " 
+echo -e ${W} " "
+
+#### About Product ####
+echo -ne ${BL1} "	-----  Product Information  -----"
+echo -e ${W}  "  \n  "
+echo -e "	        Product Name      :  $PN_FRU"
+echo -e "	        Product Part Num. :  $PNU_FRU"
+echo -e "	        Product Serial    :  $PS_FRU"
+echo -e " \n "
+
 ##### About MB ####
 echo -ne  ${BL1} "	-----  MotherBoard Information  -----"
-echo -e ${W}"   \n "
-echo -ne "		MB Product Name :  $MPM"
-echo -e " "
-echo -ne "		MB Serial Number  :  $MSN" 
-#echo -e " " 
-#echo -ne "		MB Version  :   $MBV" 
+echo -e ${W}  "   \n "
+echo -e "		MotherBoard       :  $MP_FRU"
+echo -e "		Serial Number     :  $MS_FRU"
+echo -e "		Manufaction Date  :  $MMF_FRU"
 echo -e "  \n "
 
 #### About BIOS ####
 echo -ne ${BL1} "	-----  BIOS Information  -----"
-echo -e ${W}  "  \n  " 
-#echo -ne "		BIOS Vendor :   $BVEN"
-#echo -e " "
-echo -ne "		BIOS Version :  $BVER"
-echo -e " "
-#echo -ne " 		BIOS Releade Date :    $BREL"
-#echo -e " "
+echo -e ${W}  "  \n  "
+echo -e "		BIOS FW Version   :  $BVER"
+echo -e " 		BIOS Releade Date :  $BREL"
 echo -e " \n "
 
 #### About BMC ####
-
 echo -ne ${BL1} "	-----  BMC Informaton  -----"
 echo -e ${W} "	\n "
-echo -ne "		IP Address BMC  :  \t\t\t$IP_BMC"
-echo -e " " 
-echo -ne "		Product Number :  $PN_BMC"
+echo -e "		BMC FW Version   :  $BMCVER"
+echo -e "		IP Address BMC   :  $IP_BMC"
+echo -e " 		BMC MAC 1        :  $M1_BMC"
+echo -e " 		BMC MAC 2        :  $M2_BMC"
 echo -e " "
-echo -ne ${BL} "	MAC Address BMC "
-echo -e ${W} " " 
-echo -e " 		MAC  Lan 1   :  $M1_BMC"
-echo -e " 		MAC  Lan 8   :  $M8_BMC"
-echo -e "               "
-echo -e "           Product Name     :  $PN_FRU"
-echo -e "              "
-echo -e "           Product Part Num.:  $PNU_FRU"
-echo -e "               "
-echo -e "           Product Version  :  $PV_FRU"
-echo -e "               "
-echo -e "           Product Serial   :  $PS_FRU"
+echo -e " "
+echo -e " \n "
+
+
 
 #ipmitool lan print 8
 
