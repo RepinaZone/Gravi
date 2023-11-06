b0ec392ffb22f98675b80d0a57152fc8b7eb8087
RepinaZona <An.repina.na@gmail.com>
2023-11-06 19:20:41 +0300

    Add file

--- updates/Tu_FRU.sh
+++ updates/Tu_FRU.sh
@@ -26,10 +26,48 @@ VAR1_A
 #read -p " Please enter the Chassis Extra" CEX
 #read -p " Please enter the Board Manufacturer " BMF
 #read -p " Please enter the Board Product Name" BPN
-#read -p " Please enter the Board Serial Number " BSN
+
+VAR2(){
+read -p " Please enter the Board Serial Number " BSN
+}
+VAR2
+VAR2_A(){
+B=`echo "$BSN" |tr -d '\n' | wc -c`
+echo "$B"
+if (( B == 10))
+ then
+   echo "OK"
+   echo "./FRUTool_2.02S id=0 BSN=""$BSN"
+ else
+   echo " Try again"
+   VAR2
+   VAR2_A
+fi
+}
+VAR2_A
+
 #read -p " Please enter the Board Part Number" BPA
 #read -p " Please enter the Product Manufacturer" PMF
-#read -p " Please enter the Product Part?Model Number" PPM
-#read -p " Please enter the Product Serial Number " PSN
+#read -p " Please enter the Product Part Model Number" PPM
+
+VAR3(){
+read -p " Please enter the Product Serial Number " PSN
+}
+VAR3
+VAR3_A(){
+C=`echo "$PSN" |tr -d '\n' | wc -c`
+echo "$C"
+if (( C == 10))
+ then
+   echo "OK"
+   echo "./FRUTool_2.02S id=0 PSN=""$PSN"
+ else
+   echo " Try again"
+   VAR3
+   VAR3_A
+fi
+}
+VAR3_A
+
 #read -p " Please enter the Product Name " PPN
 #read -p " Please enter the Product Version" PPV
